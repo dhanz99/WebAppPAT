55b0d08adbe49ff255f1efa2db9f7de744e5833a
Muhammad Hirdhan Aghnia <mhirdhanaghnia@gmail.com>
2021-01-08 08:06:27 +0700

    jadi

--- WebApplication2/WebApplication2/Default.aspx.designer.cs
+++ WebApplication2/WebApplication2/Default.aspx.designer.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WebApplication1 {
+namespace WebApplication2 {
     
     
     public partial class _Default {
